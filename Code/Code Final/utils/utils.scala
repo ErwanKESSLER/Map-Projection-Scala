4272122a83b55685f8634d2c7d4d509e3e930b42
KESSLER Erwan <erwan.kessler@telecomnancy.eu>
2018-11-30 00:45:06 +0100

    Revert "Merge branch 'Erwan' into 'master'"
    This reverts merge request !1

--- Code/Code Final/utils/utils.scala
+++
@@ -1,8 +0,0 @@
-package utils
-class utils {
-  def printAllElement2D(source: Array[(Int, String, String, String, Double, Double)]) = {
-    for (i <- source.indices) {
-      println(source(i).productIterator.mkString("; "))
-    }
-  }
-}
