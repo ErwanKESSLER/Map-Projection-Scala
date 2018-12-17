05d6e441455ddd574c978df506a1d6f2a2c4863c
Unknown <h66R10@gmail.com>
2018-12-17 13:46:56 +0100

    added a circle method (kindof, i would love to use creategraphic but its too java i think)

--- Code/Code Final/func/createImage.scala
+++ Code/Code Final/func/createImage.scala
@@ -1,7 +1,6 @@
 package func
 
 import java.awt.image.BufferedImage
-
 import scala.math._
 
 class createImage {
@@ -17,18 +16,34 @@ class createImage {
     val util = new utils.utils
     var img = util.readImage(filename)
     val (width, height): (Int, Int) = (img.getWidth, img.getHeight)
-    source.foreach(el=>{
+    source.foreach(el => {
       val (x, y): (Int, Int) = transformToXY(el._5, el._6, width, height)
-      img=addRectangle(img,x,y)
+      img = addCircle(img, x, y, 9, 0xFFFF0000)
     })
     util.writeImage(img, "final.png")
   }
-  def addRectangle(source:BufferedImage,x:Int,y:Int): BufferedImage={
-    for (i <- x - 5 until x + 5) {
-      for (j <- y - 5 until y + 5) {
-        if (i>=0 && i<source.getWidth() && j>=0 &&j<source.getHeight()){
 
-          source.setRGB(i, j, 0xffff0000) //red
+  def addRectangle(source: BufferedImage, x: Int, y: Int, size: Int, color: Int): BufferedImage = {
+    val middle = size / 2
+    for (i <- x - middle until x + middle + size % 2) {
+      for (j <- y - middle until y + middle + size % 2) {
+        if (i >= 0 && i < source.getWidth() && j >= 0 && j < source.getHeight()) {
+
+          source.setRGB(i, j, color)
+        }
+      }
+    }
+    source
+  }
+
+  def addCircle(source: BufferedImage, x: Int, y: Int, size: Int, color: Int): BufferedImage = {
+    val middle = size / 2
+    for (i <- x - middle until x + middle + size % 2) {
+      for (j <- y - middle until y + middle + size % 2) {
+        if (abs(i - x) + abs(j - y) <= middle) { //circular condition
+          if (i >= 0 && i < source.getWidth() && j >= 0 && j < source.getHeight()) {
+            source.setRGB(i, j, color)
+          }
         }
       }
     }
