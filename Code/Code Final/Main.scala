adde0913ed7d9ad120f625e8e3bb928b19b659b6
Erwan <erwan.kessler@gmail.com>
2018-12-18 13:51:35 +0100

    linting why????

--- Code/Code Final/Main.scala
+++ Code/Code Final/Main.scala
@@ -1,51 +1,54 @@
 package runtime
+
 import func.loadAirports
 import utils.utils
 import func.distanceAirports
-object Main extends App{
-  def Main()={
-      val loadAirport=new func.loadAirports
-      val distance=new func.distanceAirports
-      val stats=new func.statsAirports
-      val util = new utils.utils
-      val restriction=new func.restrictArea
-      val density=new func.densityAirports
-      val image=new func.createImage
-      //Example of use of step 1 code
-      val airports=loadAirport.loadAirport(filename = "airports.dat")
-      /*density.loadCSV("populations.csv")
-      println(util.showAllCountries(airports).mkString("\n"))
-      println(util.countriesCodeTable())*/
-      image.modifyImage("equirectangular.png",airports)
-      //util.problematicCountries(airports)
-      //util.notOfficialNametoAlpha3(airports)
-      println(density.Densite(airports,"populations.csv"))
-      /*
-       //util.printAllElement2DTuple(airports)
-      //Example of distance between 2 airports
-      val (lat1,lat2,lon1,lon2)=(-6.081689834590001,-5.20707988739,145.391998291,145.789001465)
-      println(distance.distanceHaversine(lat1,lat2,lon1,lon2).toString+" km")
-      println("country")
-      val countries=restriction.byCountry(airports,Set("France","Spain"))
-      println(countries)
-      util.printAllElement2DTuple(countries)
-      println("area")
-      val area=restriction.byArea(airports,(20,20),(-20,-20))
-      util.printAllElement2DTuple(area)
-      println("radius")
-      val radius=restriction.byRadius(airports,(0,0),1000)
-      util.printAllElement2DTuple(radius)
-      //Example of creating the distance array
-      val distances=distance.distancesArray(airports)
-      //util.printAllElement2DDouble(distances)
-      //Example Stats
-     println(stats.distanceMoyenne(distances))
-      println(stats.distanceMax(distances))
-      println(stats.distanceMin(distances))
-      println(stats.ecartType(distances))
-      //println(stats.distanceMediane1(distances))
-      println(stats.distanceMediane2(distances))
-      */
+
+object Main extends App {
+  def Main() = {
+    val loadAirport = new func.loadAirports
+    val distance = new func.distanceAirports
+    val stats = new func.statsAirports
+    val util = new utils.utils
+    val restriction = new func.restrictArea
+    val density = new func.densityAirports
+    val image = new func.createImage
+    //Example of use of step 1 code
+    val airports = loadAirport.loadAirport(filename = "airports.dat")
+    /*density.loadCSV("populations.csv")
+    println(util.showAllCountries(airports).mkString("\n"))
+    println(util.countriesCodeTable())*/
+    image.modifyImage("equirectangular.png", airports)
+    //util.problematicCountries(airports)
+    //util.notOfficialNametoAlpha3(airports)
+    println(density.Densite(airports, "populations.csv"))
+    /*
+     //util.printAllElement2DTuple(airports)
+    //Example of distance between 2 airports
+    val (lat1,lat2,lon1,lon2)=(-6.081689834590001,-5.20707988739,145.391998291,145.789001465)
+    println(distance.distanceHaversine(lat1,lat2,lon1,lon2).toString+" km")
+    println("country")
+    val countries=restriction.byCountry(airports,Set("France","Spain"))
+    println(countries)
+    util.printAllElement2DTuple(countries)
+    println("area")
+    val area=restriction.byArea(airports,(20,20),(-20,-20))
+    util.printAllElement2DTuple(area)
+    println("radius")
+    val radius=restriction.byRadius(airports,(0,0),1000)
+    util.printAllElement2DTuple(radius)
+    //Example of creating the distance array
+    val distances=distance.distancesArray(airports)
+    //util.printAllElement2DDouble(distances)
+    //Example Stats
+   println(stats.distanceMoyenne(distances))
+    println(stats.distanceMax(distances))
+    println(stats.distanceMin(distances))
+    println(stats.ecartType(distances))
+    //println(stats.distanceMediane1(distances))
+    println(stats.distanceMediane2(distances))
+    */
   }
+
   Main()
 }
--- Code/Code Final/func/statsAirports.scala
+++ Code/Code Final/func/statsAirports.scala
@@ -1,71 +1,68 @@
 package func
+
 import math._
 import scala.util.Random
 
 
 class statsAirports {
-
-    def quickSelect(source:Array[Double],n:Int, rand: Random = new Random): Double = {
-        val pivot = rand.nextInt(source.length);
-        val (left, right) = source.partition(_ < source(pivot))
-        if (left.length == n) {
-            source(pivot)
-        } 
-        else if (left.length < n) {
-            quickSelect(right, n - left.length, rand)
-        } 
-        else {
-            quickSelect(left, n, rand)
-        }
+  def quickSelect(source: Array[Double], n: Int, rand: Random = new Random): Double = {
+    val pivot = rand.nextInt(source.length);
+    val (left, right) = source.partition(_ < source(pivot))
+    if (left.length == n) {
+      source(pivot)
     }
-
-
-
-
-    def distanceMin(source:Array[Double]):Double={
-        var mini:Double=source(0)
-        for ( i <- source.indices ) {
-            if ( mini > source(i) ) {
-                mini=source(i)
-            }
-        }
-        mini
+    else if (left.length < n) {
+      quickSelect(right, n - left.length, rand)
     }
-
-    def distanceMax(source:Array[Double]):Double={
-        var maxi:Double=0
-        for ( i <- source.indices ) {
-            if ( maxi < source(i) ) {
-                maxi=source(i)
-            }
-        }
-        maxi
+    else {
+      quickSelect(left, n, rand)
     }
+  }
 
-    def distanceMoyenne(source:Array[Double]):Double={
-        var moyenne:Double=0
-        for (i <- source.indices ) {
-            moyenne+=source(i)
-        }
-        moyenne/source.length
+  def distanceMin(source: Array[Double]): Double = {
+    var mini: Double = source(0)
+    for (i <- source.indices) {
+      if (mini > source(i)) {
+        mini = source(i)
+      }
     }
+    mini
+  }
 
-    def ecartType(source:Array[Double]):Double={
-        val moyenne:Double=distanceMoyenne(source)
-        var s:Double=0
-        for (i <- source.indices ) {
-            s+= pow(source(i) - moyenne,2)
-        }
-        sqrt(s/(source.length-1))
+  def distanceMax(source: Array[Double]): Double = {
+    var maxi: Double = 0
+    for (i <- source.indices) {
+      if (maxi < source(i)) {
+        maxi = source(i)
+      }
     }
+    maxi
+  }
 
-    def distanceMediane1(source:Array[Double]):Double={
-        val res:Array[Double]=source.sorted
-        res(res.length/2)
+  def distanceMoyenne(source: Array[Double]): Double = {
+    var moyenne: Double = 0
+    for (i <- source.indices) {
+      moyenne += source(i)
     }
+    moyenne / source.length
+  }
 
-    def distanceMediane2(source:Array[Double]):Double={ // Plus rapide que Mediane1
-        quickSelect(source,source.length/2)
-        }
+  def ecartType(source: Array[Double]): Double = {
+    val moyenne: Double = distanceMoyenne(source)
+    var s: Double = 0
+    for (i <- source.indices) {
+      s += pow(source(i) - moyenne, 2)
+    }
+    sqrt(s / (source.length - 1))
+  }
+
+  def distanceMediane1(source: Array[Double]): Double = {
+    val res: Array[Double] = source.sorted
+    res(res.length / 2)
+  }
+
+  def distanceMediane2(source: Array[Double]): Double = { // Plus rapide que Mediane1
+    quickSelect(source, source.length / 2)
+  }
 
 }
